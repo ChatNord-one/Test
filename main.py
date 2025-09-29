65473566e48a564e26321e4c951e0fbb8e404769
Jerik <khatnord@gmail.com>
2025-09-29 14:48:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,1 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return """
-    <h1>Привіт! 👋</h1>
-    <p>Я навчаюсь створювати сайти на Python.</p>
-    <p>Це моя перша сторінка!</p>
-    """
-
-if __name__ == "__main__":
-    app.run(debug=True)
-  
+# dfsd
